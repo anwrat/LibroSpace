bf5c401cf8e0cb1ebdd2f429af964407cc76fee8
anwrat <anweshrwt22@gmail.com>
2025-11-08 15:27:16 +0545

    Creating a users table using sql

--- backend/index.js
+++ backend/index.js
@@ -1,23 +1,9 @@
 import app from './app.js';
 import dotenv from 'dotenv';
 dotenv.config();
-import pkg from 'pg';
-
-const {Pool} = pkg;
-
-const pool = new Pool({
-    connectionString: process.env.DATABASE_URL,
-})
 
 const PORT = process.env.PORT;
 
 app.listen(PORT,()=>{
     console.log(`Server running on port ${PORT}`);
 })
-
-try{
-    const res = await pool.query('SELECT NOW()');
-    console.log('Database connected at ',res.rows[0].now);
-}catch(err){
-    console.error('Database connection error',err);
-}
