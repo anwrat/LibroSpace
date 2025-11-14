63b1da8f757a6609fa16d051fea2b8bf4066f5f9
anwrat <anweshrwt22@gmail.com>
2025-11-14 12:27:38 +0545

    Register api functioning

---
+++ backend/sql/alterautoincrementfieldsequence.sql
@@ -0,0 +1,1 @@
+ALTER SEQUENCE auth.users_id_seq RESTART WITH 1;
--- backend/src/models/users.model.ts
+++ backend/src/models/users.model.ts
@@ -9,11 +9,11 @@ export interface User{
 }
 
 export const createUser = async (name: string, email: string, password: string): Promise<User>=>{
-    const result = await pool.query('INSERT INTO users (name, email, password) VALUES ($1,$2,$3) RETURNING *', [name, password, email]);
+    const result = await pool.query('INSERT INTO auth.users (name, email, password) VALUES ($1,$2,$3) RETURNING *', [name, email, password]);
     return result.rows[0];
 }
 
 export const findUserByEmail = async (email:string): Promise<User> =>{
-    const result = await pool.query('SELECT * FROM users WHERE email = $1', [email]);
+    const result = await pool.query('SELECT * FROM auth.users WHERE email = $1', [email]);
     return result.rows[0];
 }
