96e2d21fb019a435802ed7b5c1889520d228beae
anwrat <anweshrwt22@gmail.com>
2025-11-15 16:42:41 +0545

    Installing material UI library

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,13 +1,15 @@
 import Image from "next/image";
+import LandingNav from "@/components/Navbar/LandingNav";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-white font-sans">
+    <div className="flex min-h-screen items-center justify-center bg-white">
+      <LandingNav />
       <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white sm:items-start">
         <Image
           className=""
           src="/Landing/centreguy.png"
-          alt="Next.js logo"
+          alt="Center Guy"
           width={100}
           height={20}
           priority
@@ -15,7 +17,7 @@ export default function Home() {
         <Image
           className=""
           src="/Landing/girl1.png"
-          alt="Next.js logo"
+          alt="Girl 1"
           width={100}
           height={20}
           priority
@@ -23,7 +25,7 @@ export default function Home() {
         <Image
           className=""
           src="/Landing/girl2.png"
-          alt="Next.js logo"
+          alt="Girl 2"
           width={100}
           height={20}
           priority
@@ -31,13 +33,13 @@ export default function Home() {
         <Image
           className=""
           src="/Landing/guy1.png"
-          alt="Next.js logo"
+          alt="Guy 2"
           width={100}
           height={20}
           priority
         />
         {/* font-main uses Inter font */}
-        <h1 className="text-4xl font-bold font-main">Because books are better when shared!</h1>
+        <h1 className="text-5xl font-semibold font-main">Because books are better when shared!</h1>
         <p className="italic text-xl font-main">Track your reads, join discussions and celebrate the joy of reading</p>
       </main>
     </div>
---
+++ frontend/src/components/Buttons/LoginButton.tsx
@@ -0,0 +1,9 @@
+'use client';
+
+export function ButtonDemo() {
+  return (
+    <div className="flex flex-wrap items-center gap-2 md:flex-row">
+      <Button variant="outline">Button</Button>
+    </div>
+  )
+}
