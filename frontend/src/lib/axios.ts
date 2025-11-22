7b5d73455d657cd0f04556fdae91645b7f72bcdc
anwrat <anweshrwt22@gmail.com>
2025-11-22 21:48:01 +0545

    Landing page frontend images adjustment

--- frontend/src/lib/axios.ts
+++ frontend/src/lib/axios.ts
@@ -4,6 +4,7 @@ const BASE = process.env.API_BASE;
 
 export const api = axios.create({
     baseURL: BASE,
+    withCredentials: true,
     headers:({
         'Content-Type': 'application/json'
     })
