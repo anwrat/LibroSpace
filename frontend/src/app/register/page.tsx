43a8ab1a16184a4f30a3f5ff98d465861ce84ba4
anwrat <anweshrwt22@gmail.com>
2025-11-23 06:49:51 +0545

    Some changes in frontend

--- frontend/src/app/register/page.tsx
+++ frontend/src/app/register/page.tsx
@@ -1,21 +1,26 @@
 'use client';
 import LandingNav from "@/components/Navbar/LandingNav";
 import Image from "next/image";
+import RegisterForm from "@/components/Forms/RegisterForm";
+import {motion} from 'framer-motion';
 
 export default function Register(){
     return(
         <div className="flex min-h-screen items-center justify-center bg-white">
             <LandingNav />
             {/* Image and form section */}
-            <div>
-                <Image
-                className=""
-                src="/Register/boyundertree.png"
-                alt="Boy Under Tree"
-                width={700}
-                height={20}
-                priority
-                />
+            <div className="w-full max-w-5xl flex flex-row items-center">
+                <motion.div initial={{y:-100, opacity:0}} animate={{opacity:1,y:0}}>
+                    <Image
+                    className=""
+                    src="/Register/boyundertree.png"
+                    alt="Boy Under Tree"
+                    width={500}
+                    height={20}
+                    priority
+                    />
+                </motion.div>
+                <RegisterForm/>
             </div>
         </div>
     )
---
+++ frontend/src/components/Forms/RegisterForm.tsx
@@ -0,0 +1,7 @@
+export default function RegisterForm(){
+    return(
+        <div>
+            <p className="font-main font-semibold text-4xl">Sign Up</p>
+        </div>
+    )
+}
