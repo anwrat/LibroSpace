f8ce49ee1fb62f7c89ddba249350273d223a6c2d
anwrat <anweshrwt22@gmail.com>
2025-11-28 20:27:24 +0545

    Google OAuth backend done

--- backend/src/controllers/oauth.controller.ts
+++ backend/src/controllers/oauth.controller.ts
@@ -1,13 +1,18 @@
 import type{ Request, Response } from "express";
-import dotenv from 'dotenv';
-dotenv.config();
 
-//Redirect to google
-export const googleOAuthHandler = async (req: Request, res: Response)=>{
+//Redirect after succesful Google OAuth Login
+export const googleAuthRedirect = async (req: Request, res: Response)=>{
     try{
-
+        const token = (req.user as any).token;
+        
+        res.cookie("token",token,{
+            httpOnly: true,
+            secure: false,
+            sameSite: "lax",
+        });
+        return res.redirect(`${process.env.CLIENT_URL}/dashboard`);
     }catch(err){
-        console.error("Error while redirecting to google",err);
-        res.status(500).json({message:"Internal Server Error while redirecting to google"});
+        console.error("Error while redirecting from google",err);
+        res.status(500).json({message:"Internal Server Error while redirecting from google"});
     }
 }
---
+++ backend/src/middleware/passport.middleware.ts
@@ -0,0 +1,38 @@
+import passport from 'passport';
+import { Strategy as GoogleStrategy } from 'passport-google-oauth20';
+import dotenv from 'dotenv';
+import { findUserByEmail, createGoogleUser } from '../models/users.model.js';
+import { signToken } from '../utils/jwt.js';
+
+dotenv.config();
+
+passport.use(new GoogleStrategy(
+    {
+        clientID: process.env.GOOGLE_CLIENT_ID as string,
+        clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+        callbackURL: process.env.GOOGLE_CALLBACK_URL as string,
+    },
+    async(accessToken,refreshToken,profile,done)=>{
+        const email = profile.emails?.[0]?.value!;
+        const picture = profile.photos?.[0]?.value || null ;
+
+        let user = await findUserByEmail(email);
+
+        if(!user){
+            user = await createGoogleUser(
+                profile.displayName,
+                email,
+                profile.id,
+                picture
+            );
+        }
+        const token = signToken({
+            id: user.id,
+            email:user.email,
+            name: user.name
+        });
+        done(null, {user,token});
+    })
+);
+
+export default passport;
