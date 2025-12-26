fbb98c13f9341401261fc0affd3d7b18e4b51d4f
anwrat <anweshrwt22@gmail.com>
2025-12-26 20:04:51 +0545

    Conditional props for login button in landingnav

--- frontend/src/app/login/page.tsx
+++ frontend/src/app/login/page.tsx
@@ -7,7 +7,7 @@ import {motion} from 'framer-motion';
 export default function Login(){
     return(
         <div className="flex min-h-screen items-center justify-center bg-white">
-            <LandingNav />
+            <LandingNav showLoginButton={false}/>
             <motion.div className="w-full max-w-5xl flex flex-row items-center gap-20" initial={{y:-100, opacity:0}} animate={{opacity:1,y:0}}>
                 <Image src='/Login/girl.png' alt='Girl reading book' width={400} height={200}/>
                 <LoginForm/>
--- frontend/src/components/Navbar/LandingNav.tsx
+++ frontend/src/components/Navbar/LandingNav.tsx
@@ -2,14 +2,18 @@
 import Image from "next/image";
 import LoginButton from "@/components/Buttons/LoginButton";
 
-export default function LandingNav(){
+interface LandingNavProps{
+    showLoginButton?: boolean;
+}
+
+export default function LandingNav({showLoginButton = true}: LandingNavProps){
     return(
         <div className="flex items-center fixed top-2 w-full max-w-6xl justify-between">
             <div className="flex flex-row items-center gap-3">
                 <Image src="/Logo/Logonobgcropped.png" width={50} height={20} alt="LibroSpace Logo"></Image>
                 <p className="font-main font-bold">LibroSpace</p>
             </div>
-            <LoginButton />
+            {showLoginButton && <LoginButton />}
         </div>
     )
 }
