86c8c30e25c09d50b0bc896f036ff49193436c0f
anwrat <anweshrwt22@gmail.com>
2025-12-28 21:12:09 +0545

    createBook route in backend done

--- backend/src/models/booklist.model.ts
+++ backend/src/models/booklist.model.ts
@@ -1,5 +1,6 @@
 import pool from "../config/db.js";
 
-export const createBook = async () =>{
-    
+export const createBook = async (title: string, author: string, description: string, cover_url: string, published_date: string, language: string, created_by: number) =>{
+    const result = await pool.query('INSERT INTO books.booklist (title, author, description, cover_url, published_date, language, created_by) VALUES ($1,$2,$3,$4,$5,$6,$7) RETURNING *', [title, author, description, cover_url, published_date, language, created_by]);
+    return result.rows[0];
 }
