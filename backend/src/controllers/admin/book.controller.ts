06ff90d8b2a7ec5b14fcae97d7b118de5f8cb30e
anwrat <anweshrwt22@gmail.com>
2026-01-03 20:58:08 +0545

    Creating a separate book duplicate checking route

--- backend/src/controllers/admin/book.controller.ts
+++ backend/src/controllers/admin/book.controller.ts
@@ -1,6 +1,5 @@
 import type{ Request, Response } from "express";
-import { createBook } from "../../models/books/booklist.model.js";
-import { getAllBooks } from "../../models/books/booklist.model.js";
+import { createBook,getAllBooks, findBookByTitleandAuthor } from "../../models/books/booklist.model.js";
 
 export const addNewBook = async (req: Request, res: Response) =>{
     try{
@@ -17,11 +16,22 @@ export const addNewBook = async (req: Request, res: Response) =>{
         const newBook = await createBook(title, author, description, cover_url, published_date, language, created_by);
         return res.status(201).json({message: "New book added successfully", details: newBook});
     }catch(err: any){
-        if(err.code === '23505' || err.message.includes('unique constraint')){
-            return res.status(409).json({message:"This books already exists"});
-        }
         console.error(err);
         res.status(500).json({message:  "Internal Server Error while adding new book"});
+    }
+}
+
+export const checkifBookExists = async(req: Request, res: Response)=>{
+    try{
+        const {title,author} = req.body;
+        const book = await findBookByTitleandAuthor(title,author);
+        if(book){
+            return res.status(200).json({message: "Book already exists"});
+        }
+        return res.status(404).json({message: "Book does not exist"});
+    }catch(err){
+        console.error(err);
+        res.status(500).json({message: "Internal Server Error while checking if book exists"});
     }
 }
 
--- backend/src/routes/admin/admin.routes.ts
+++ backend/src/routes/admin/admin.routes.ts
@@ -2,7 +2,7 @@ import { authorizeAdmin } from "../../middleware/auth/admin.middleware.js";
 import { authenticateToken } from "../../middleware/auth/auth.middleware.js";
 import { Router } from "express";
 import { fetchAllUsers } from "../../controllers/admin/users.controller.js";
-import { addNewBook,fetchAllBooks } from "../../controllers/admin/book.controller.js";
+import { addNewBook,fetchAllBooks, checkifBookExists } from "../../controllers/admin/book.controller.js";
 import { bookImgUpload } from "../../middleware/imgupload/bookimg.middleware.js";
 
 const router = Router();
@@ -10,7 +10,8 @@ const router = Router();
 router.get('/users',authenticateToken, authorizeAdmin, fetchAllUsers)
 
 // Books routes
-router.post('/books/add',authenticateToken, authorizeAdmin, bookImgUpload.single('cover'),addNewBook)
+router.post('/books/add',authenticateToken, authorizeAdmin, bookImgUpload.single('cover'),addNewBook);
+router.post('/books/check',authenticateToken,authorizeAdmin,checkifBookExists);
 router.get('/books',authenticateToken,authorizeAdmin,fetchAllBooks);
 
 export default router;
