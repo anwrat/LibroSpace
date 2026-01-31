1443009a28796c5bbd90f3ef6a0f34e4930ea7ac
anwrat <anweshrwt22@gmail.com>
2026-01-31 21:12:47 +0545

    Modifying User Navbar to show user profile picture

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -10,6 +10,12 @@ const nextConfig: NextConfig = {
         hostname: 'res.cloudinary.com',
         pathname: `/**`,
       },
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
