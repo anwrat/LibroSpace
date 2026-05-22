63e61f80315bd1da24482b18607152abb95f8e8c
anwrat <anweshrwt22@gmail.com>
2026-05-22 21:12:29 +0545

    Community rooms now ending when host clicks end

--- backend/src/models/communities/community_rooms.model.ts
+++ backend/src/models/communities/community_rooms.model.ts
@@ -1,7 +1,7 @@
 import pool from "../../config/db.js";
 
 export const getActiveRoomsByCommunityId = async(community_id: number)=>{
-    const query = `SELECT *, b.title as book_title, b.* FROM communities.community_rooms cr JOIN books.booklist b ON cr.book_id = b.id WHERE cr.community_id = $1 AND cr.is_active = true`;
+    const query = `SELECT cr.id as room_id, cr.*, b.title as book_title, b.* FROM communities.community_rooms cr JOIN books.booklist b ON cr.book_id = b.id WHERE cr.community_id = $1 AND cr.is_active = true`;
     const result = await pool.query(query, [community_id]);
     return result.rows;
 }
@@ -9,5 +9,11 @@ export const getActiveRoomsByCommunityId = async(community_id: number)=>{
 export const startNewRoom = async(community_id: number, host_id: number, book_id: number)=>{
     const query = `INSERT INTO communities.community_rooms (community_id, host_id, book_id) VALUES ($1, $2, $3) RETURNING *`;
     const result = await pool.query(query, [community_id, host_id, book_id]);
+    return result.rows[0];
+}
+
+export const endRoom = async(room_id: number)=>{
+    const query = `UPDATE communities.community_rooms SET is_active = false WHERE id = $1 RETURNING *`;
+    const result = await pool.query(query, [room_id]);
     return result.rows[0];
 }
